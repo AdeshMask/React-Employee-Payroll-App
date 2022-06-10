d372d28f322f7f176436ef36433b2150f436ce7e
AdeshMask <adeshmaske255@gmail.com>
2022-06-11 01:02:54 +0530

    Added:Routing When Event occures

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/add" element={<Add />} />
         </Routes>
       </BrowserRouter>
